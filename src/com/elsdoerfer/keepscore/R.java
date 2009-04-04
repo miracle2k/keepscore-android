1e1ac9886acc4ec48f97ef2babcbf70c19a963b7
melsdoerfer <michael@elsdoerfer.com>
2009-04-04 11:02:07 +0200

    Added an icon for the 'new player' button.

--- src/com/elsdoerfer/keepscore/R.java
+++ src/com/elsdoerfer/keepscore/R.java
@@ -14,7 +14,8 @@ public final class R {
         public static final int game_table_padding=0x7f040000;
     }
     public static final class drawable {
-        public static final int icon=0x7f020000;
+        public static final int ic_menu_add_small=0x7f020000;
+        public static final int icon=0x7f020001;
     }
     public static final class id {
         public static final int add_new_player=0x7f070006;
